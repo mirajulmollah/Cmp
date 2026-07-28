5ba8ce0f13d828a7e3b8519c963858c509417065
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-07-28 20:20:52 +0530

    first commit

---
+++ Component/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='container'>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ Component/src/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return (
+        <div className="card">
+            <h1>Title</h1>
+            <p className="description">Desciption</p>
+        </div>
+            )
+}
+            export default Card
