56ee67103d66f9e1e45945063bb3a54686a7c558
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-07-29 00:08:07 +0530

    all ok

--- Component/src/App.jsx
+++ Component/src/App.jsx
@@ -1,3 +1,7 @@
+//============================================================App.jsx
+
+
+
 import { useState } from 'react'
 import './App.css'
 import Card from './Card'
@@ -8,12 +12,13 @@ function App() {
   return (
     <>
     <div className='container'>
+    <Card title="frist" description="This is my frist"/> 
     <Card/>
     <Card/>
     <Card/>
     <Card/>
-    <Card/>
-    <Card/>
+    <Card title="Seconed" description="This is my seconed"/>
+    
     </div>
     </>
   )
--- Component/src/Card.jsx
+++ Component/src/Card.jsx
@@ -1,9 +1,22 @@
-function Card() {
+//====================================================Card.jsx
+
+import { useState } from "react"
+
+
+
+function Card(props) {
+    const [count, setCount] = useState(0)
+    function Increment() {
+        setCount(count + 1)
+    }
     return (
         <div className="card">
-            <h1>Title</h1>
-            <p className="description">Desciption</p>
+            <h2>{count}</h2>
+            <button onClick={Increment}>Increment</button>
+            <h1>{props.title}</h1>
+            <p className="description">{props.description}</p>
         </div>
-            )
+    )
 }
-            export default Card
+export default Card
+
